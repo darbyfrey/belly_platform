5f0673a961a4c70dbbf9ee2dc0dec3e0cf8dcd03
Jay OConnor <jay@bellycard.com>
2013-07-08 10:55:33 -0500

    cache buster

--- belly_platform.gemspec
+++ belly_platform.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |gem|
 
   gem.add_dependency 'rake'
   gem.add_dependency 'logging'
+  gem.add_dependency 'grape'
   gem.add_development_dependency "rspec"
   gem.add_development_dependency "pry"
 end
--- lib/belly_platform.rb
+++ lib/belly_platform.rb
@@ -2,6 +2,7 @@ require "belly_platform/version"
 require "belly_platform/logger"
 require "belly_platform/identity"
 require "belly_platform/middleware/logger"
+require "belly_platform/middleware/cache_buster"
 
 module BellyPlatform
   # Your code goes here...
