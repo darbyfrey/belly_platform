8f2d6926f7624d09d69a95dc9a29db75823548bb
Darby Frey <darbyfrey@gmail.com>
2013-08-01 21:58:04 -0500

    Adding more validations

--- lib/belly_platform/string.rb
+++ lib/belly_platform/string.rb
@@ -3,18 +3,22 @@ module BellyPlatform
     class << self
       def validate(type, value)
         case type
-        when 'integer'
+        when 'integer', 'int'
           !!(value =~ /^-?\d*(,\d*)*$/)
         when 'boolean'
           BellyPlatform::Boolean.is_boolean?(value)
         when 'timestamp'
-          !!(value =~ /^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})$|^(\d{10})$/)
-        when 'array_of_integers'
+          !!(value =~ /^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}($|(\.\d+)$))|^(\d{10})$/)
+        when 'double'
+          !!(value =~ /^-?\d*.\d*$/)
+        when 'array_of_integers', 'array_of_ints'
           value = value.split(',')
           value.is_a?(Array) && value.all?{|i| !!(i =~ /^\d*(,\d*)*$/)}
         when 'array_of_strings'
           value = value.split(',')
           value.is_a?(Array) && value.all?{|i| !!(i =~ /^[.\d\w-][.\d\w-]*(,[.\d\w-][.\d\w-]*)*$/)}
+        when 'snake_case'
+          !!(value =~ /^[a-zA-Z0-9_-]+$/)
         end 
       end
 
