b70fe3f43e1133aa8a9fd4a200106a2b92e29b3c
Jay OConnor <jay@bellycard.com>
2013-10-16 10:40:31 -0500

    removing the root from json objects

--- lib/generators/templates/scaffold/config/initializers/active_record.rb
+++ lib/generators/templates/scaffold/config/initializers/active_record.rb
@@ -2,3 +2,4 @@ require 'erb'
 db = YAML.load(ERB.new(File.read('./config/database.yml')).result)[BellyPlatform.env]
 ActiveRecord::Base.establish_connection(db)
 ActiveRecord::Base.logger = BellyPlatform::Logger.logger if BellyPlatform.env.development?
+ActiveRecord::Base.include_root_in_json = false
